f823b54e29247af2e45e18d86bceb0b1e55b0dce
VulpyWags <33859387+VulpyWags@users.noreply.github.com>
2026-04-12 10:40:46 +0200

    Add small delay to ghost beat rewards
    Fixes an issue with overlapping +GC on screen logs

--- resources/[race]/race_ghost/playback_server.lua
+++ resources/[race]/race_ghost/playback_server.lua
@@ -385,8 +385,10 @@ addEventHandler("onPlayerFinish", root, function(rank, time)
 		end
 	end
 	if nBeatenGhosts == 0 then return end
-	outputChatBox("You beat " .. nBeatenGhosts .. " ghost(s) on this map and earned " .. nBeatenGhosts .. " bonus GC!", source, 255, 255, 0)
-	exports.gc:addPlayerGreencoins(source, nBeatenGhosts)
+	setTimeout(function() -- small delay so UI +GC log doesn't overlapped
+		outputChatBox("You beat " .. nBeatenGhosts .. " ghost(s) on this map and earned " .. nBeatenGhosts .. " bonus GC!", source, 255, 255, 0)
+		exports.gc:addPlayerGreencoins(source, nBeatenGhosts)
+	end, 1500, 1)
 end)
 
 addEvent("onClientRequestPBGhost", true)
